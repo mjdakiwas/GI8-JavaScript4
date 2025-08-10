41a08472b03a91afe8955f8b254b0333c2c3860e
Marlette Dakiwas <mjessdakiwas@gmail.com>
2025-08-10 05:17:17 -0400

    pulled to main branch and made readme file

---
+++ README.md
@@ -0,0 +1,21 @@
+# Guided Inquiry 9 - SASS & JavaScript 4
+## Budget Tracker
+### How to Use
+#### Mobile View (<768px)
+1. Click the plus button to open the new transaction form.
+2. Select the transaction type (income or expense).
+3. Input the transaction's description or name.
+4. Input the transaction's amount, which must be a number.
+** All input fields must have a value in order to continue. **
+5. Click the "Add Transaction" button to add the transaction into the budget tracker.
+
+##### To view the total balance, total income, or total expense:
+- Click the indicator at the bottom of the overview section (underneath the total amount).
+- Click the left or right button to go to the previous or next slide.
+
+#### Wider-screen View (>768px)
+1. Select the transaction type (income or expense).
+3. Input the transaction's description or name.
+4. Input the transaction's amount, which must be a number.
+** All input fields must have a value in order to continue. **
+5. Click the "Add Transaction" button to add the transaction into the budget tracker.
--- script.js
+++ script.js
@@ -1,328 +1,3 @@
-// // budget functionality
-// class Budget {
-//     constructor(budget, transactionList = [], incomeList = [], expenseList = []) {
-//         this.budget = budget;
-//         this.transactionList = transactionList;
-//         this.incomeList = incomeList;
-//         this.expenseList = expenseList;
-//     }
-
-//     addTransaction(transaction) {
-//         // this method will add the transaction into the array
-//         // trigger everytime "add new transaction" form is submitted
-//         this.transactionList.unshift(transaction);
-//     }
-
-//     getTransactions() {
-//         return this.transactionList;
-//     }
-
-//     // displayTransactions() {
-//     //     for (let i; i < this.transactionList.length; i++) {
-//     //         const transactionContainer = document.createElement('div');
-//     //         const transactionDescription = document.createElement('p');
-//     //         const transactionAmount = document.getElementById('p');
-
-//     //         transactionDescription.innerText = this.transactionList[i]['description'];
-//     //         transactionAmount.innerText = this.transactionList[i]['amount'];
-
-//     //         transactionContainer.appendChild(transactionDescription);
-//     //         transactionContainer.appendChild(transactionAmount);
-//     //         document.body.appendChild(transactionContainer);
-//     //     }
-//     // }
-
-//     // based on transaction type: budget += income, budget -= expense => save budget (will use to display)
-//     // save income: income += amount (will use to display)
-//     // save expense: expense += amount (will use to display)
-//     // getTotalBudget() {
-//     //     return this.budget;
-//     // }
-//     // getTotalBudget(transaction) {
-//     //     // this method will return the total budget
-//     //     // add income into total budget
-//     //     // subtract expense from total budget
-//     //     const transactionType = Object.values(transaction)[0];
-//     //     console.log(transactionType);
-//     //     const amount = Number(Object.values(transaction)[2]);
-//     //     if (transactionType === 'income') {
-//     //         console.log(`${transactionType} is an income`);
-//     //         console.log(`Current Balance: ${this.budget} + Adding ${amount}`);
-//     //         console.log(`Current Income: ${this.income} + Adding ${amount}`);
-//     //         this.budget += amount;
-//     //         this.income += amount;
-//     //         console.log(`Updated Budget: ${this.budget}; Updated Income: ${this.income}`);
-//     //     } else if (transactionType === 'expense') {
-//     //         console.log(`${transactionType} is an expense`);
-//     //         console.log(`Current Balance: ${this.budget} + Subtracting ${amount}`);
-//     //         console.log(`Current Expense: ${this.expense} + Adding ${amount}`);
-//     //         this.budget -= amount;
-//     //         this.expense += amount;
-//     //         console.log(`Updated Budget: ${this.budget}; Updated Expense: ${this.expense}`);
-//     //     }
-//     //     return this.budget;
-//     // }
-
-//     // getIncome() {
-//     //     return this.income;
-//     // }
-
-//     // getExpense() {
-//     //     return this.expense;
-//     // }
-// }
-
-// class Transaction extends Budget {
-//     constructor(budget, income, expense, transactionType, description, amount) {
-//         super(budget, income, expense);
-//         this.transactionType = transactionType;
-//         this.description = description;
-//         this.amount = amount;
-//     }
-
-//     getTotalBudget() {
-//         if (this.transactionType === 'income') {
-//             console.log(`${this.transactionType} is an income`);
-//             console.log(`Current Balance: ${this.budget} + Adding ${this.amount}`);
-//             console.log(`Current Income: ${this.income} + Adding ${this.amount}`);
-//             this.budget += this.amount;
-//             this.income += this.amount;
-//             console.log(`Updated Budget: ${this.budget}; Updated Income: ${this.income}`);
-//         } else if (this.transactionType === 'expense') {
-//             console.log(`${this.transactionType} is an expense`);
-//             console.log(`Current Balance: ${this.budget} + Subtracting ${this.amount}`);
-//             console.log(`Current Expense: ${this.expense} + Adding ${this.amount}`);
-//             this.budget -= this.amount;
-//             this.expense += this.amount;
-//             console.log(`Updated Budget: ${this.budget}; Updated Expense: ${this.expense}`);
-//         }
-//         return this.budget;
-//     }
-
-//     getIncome() {
-//         return this.income;
-//     }
-
-//     getExpense() {
-//         return this.expense;
-//     }
-
-//     getTransactionType() {
-//         return this.transactionType;
-//     }
-
-//     getDescription() {
-//         return this.description;
-//     }
-
-//     getAmount() {
-//         return this.amount;
-//     }
-// }
-
-// // on page load, create budget object
-// const addTransactionBtn = document.getElementById('expense-add');
-// const addTransactionForm = document.getElementById('new-transaction__container');
-// const cancelTransactionBtn = document.getElementById('close-new-transaction');
-// const formOveray = document.getElementById('form-overlay');
-// const balanceDisplay = document.getElementById('balance-display');
-// const transactionsDisplay = document.getElementById('transaction-display');
-
-// let budgetTracker;
-// // let transactionList;
-// let transaction; // object of form submission
-// let balance = 0, transactionList = [], income = 0, expense = 0;
-// budgetTracker = new Budget(balance, transactionList, income, expense, [], []);
-
-// addTransactionForm.addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     const transactionType = document.getElementById('transaction-type').value;
-//     const transactionDescription = document.getElementById('description').value;
-//     const transactionAmount = Number(document.getElementById('amount').value);
-
-//     const transaction = new Transaction(balance, income, expense, transactionType, transactionDescription, transactionAmount);
-//     console.log(transaction);
-//     budgetTracker.addTransaction(transaction); // add form data into transaction array (will use to display into transactions list)
-//     addTransactionForm.style.display = 'none';
-
-//     // display balance
-//     // display income
-//     // display expense
-//     balance = budgetTracker.getTotalBudget();
-//     income = budgetTracker.getIncome();
-//     expense = budgetTracker.getExpense();
-//     console.log(`Current Balance: ${balance}`);
-//     console.log(`Current Income: ${income}`);
-//     console.log(`Current Expense: ${expense}`);
-//     balanceDisplay.innerText = balance;
-//     formOveray.style.display = 'none';
-// })
-
-// // form validation
-// // every form submission => take input values as an object
-// // addTransactionForm.addEventListener('submit', (event) => {
-// //     event.preventDefault();
-// //     const formData = new FormData(addTransactionForm); // get form's input fields and their values as key/value pairs
-// //     transaction = Object.fromEntries(formData.entries()); // convert key/value pairs into an object
-// //     // console.log(transaction);
-// //     budgetTracker.addTransaction(transaction); // add form data into transaction array (will use to display into transactions list)
-// //     addTransactionForm.style.display = 'none';
-
-// //     // display balance
-// //     // display income
-// //     // display expense
-// //     balance = budgetTracker.getTotalBudget(transaction);
-// //     income = budgetTracker.getIncome();
-// //     expense = budgetTracker.getExpense();
-// //     console.log(`Current Balance: ${balance}`);
-// //     console.log(`Current Income: ${income}`);
-// //     console.log(`Current Expense: ${expense}`);
-// //     balanceDisplay.innerText = balance;
-// //     formOveray.style.display = 'none';
-// // })
-
-// // display list of transactions
-// // const displayingTransactions = budgetTracker.displayTransactions();
-// // console.log(displayingTransactions);
-// // transactionList = budgetTracker.getTransactions();
-// // console.log(transactionList);
-// // for (let i; i < transactionList.length; i++) {
-// //     const transactionContainer = document.createElement('div');
-// //     const transactionDescription = document.createElement('p');
-// //     const transactionAmount = document.getElementById('p');
-
-// //     transactionDescription.innerText = Object.values(transactionList[i][1]);
-// //     transactionAmount.innerText = Object.values(transactionList[i][2]);
-
-// //     transactionContainer.appendChild(transactionDescription);
-// //     transactionContainer.appendChild(transactionAmount);
-// //     document.body.appendChild(transactionContainer);
-// // }
-
-// // mobile ui js
-// addTransactionBtn.addEventListener('click', () => {
-//     addTransactionForm.style.display = 'flex';
-//     formOveray.style.display = 'block';
-// })
-
-// cancelTransactionBtn.addEventListener('click', () => {
-//     addTransactionForm.style.display = 'none';
-//     formOveray.style.display = 'none';
-// })
-
-// // desktop ui js
-
-
-
-
-
-// // budget class
-// class Budget {
-//     constructor(budget = 0, income = 0, expense = 0) {
-//         this.budget = budget;
-//         this.income = income;
-//         this.expense = expense;
-//     }
-
-//     getBudget() {
-//         return this.budget;
-//     }
-
-//     getIncome() {
-//         return this.income;
-//     }
-
-//     getExpense() {
-//         return this.expense;
-//     }
-// }
-
-// class Transaction extends Budget {
-//     constructor(transactionType, description, amount) {
-//         super();
-//         this.transactionType = transactionType;
-//         this.description = description;
-//         this.amount = amount;
-//     }
-
-//     // calculate total budget => budget + income or budget - expense (based on user input)
-//     calculateTotalBudget() {
-//         if (this.transactionType === 'income') {
-//             return this.getBudget() + this.amount;
-//         } else if (this.transactionType === 'expense') {
-//             return this.getBudget() - this.amount;
-//         }
-//     }
-
-//     calculateIncome() {
-//         if (this.transactionType === 'income') {
-//             return this.getIncome() + this.amount;
-//         }
-//         return;
-//     }
-
-//     calculateExpense() {
-//         if (this.transactionType === 'expense') {
-//             return this.getExpense() + this.amount;
-//         }
-//         return;
-//     }
-// }
-
-// const addTransactionBtn = document.getElementById('expense-add');
-// const addTransactionForm = document.getElementById('new-transaction__container');
-// const cancelTransactionBtn = document.getElementById('close-new-transaction');
-// const formOveray = document.getElementById('form-overlay');
-// const balanceDisplay = document.getElementById('balance-display');
-// const transactionsDisplay = document.getElementById('transaction-display');
-
-// let totalBudget, totalIncome, totalExpense;
-// const budgetTracker = new Budget(totalBudget, totalIncome, totalExpense);
-// console.log(budgetTracker);
-// let transactionList = [];
-// // let currentBudget = budgetTracker.getBudget();
-// // let currentIncome = budgetTracker.getIncome();
-// // let currentExpense = budgetTracker.getExpense();
-// addTransactionForm.addEventListener('submit', (event) => {
-//     event.preventDefault();
-
-//     const transactionType = document.getElementById('transaction-type').value;
-//     const transactionDescription = document.getElementById('description').value;
-//     const transactionAmount = Number(document.getElementById('amount').value);
-//     const transaction = new Transaction(transactionType, transactionDescription, transactionAmount);
-//     console.log(transaction);
-//     transactionList.unshift(transaction);
-//     console.log(transactionList);
-
-//     totalBudget = transaction.calculateTotalBudget();
-//     totalIncome = transaction.calculateIncome();
-//     totalExpense = transaction.calculateExpense();
-//     console.log(`Budget: ${totalBudget}; Income: ${totalIncome}; Expense: ${totalExpense}`);
-//     formOveray.style.display = 'none';
-// })
-
-// // mobile ui js
-// addTransactionBtn.addEventListener('click', () => {
-//     addTransactionForm.style.display = 'flex';
-//     formOveray.style.display = 'block';
-// })
-
-// cancelTransactionBtn.addEventListener('click', () => {
-//     addTransactionForm.style.display = 'none';
-//     formOveray.style.display = 'none';
-// })
-
-// // update total budget dynamically => update every new input
-// // incomes array or object
-// // expenses array or object
-// // form validation => can't enter empty input fields
-// // dynamically display total budget, income, expenses => update every new input
-
-
-
-
-
-
 // budget functionality
 class Budget {
     constructor(budget = 0, transactionList = [], income = 0, expense = 0, incomeList = [], expenseList = []) {
@@ -338,18 +13,6 @@ class Budget {
         // this method will add the transaction into the array
         // trigger everytime "add new transaction" form is submitted
         this.transactionList.unshift(transaction);
-    }
-
-    addIncomeTransaction(transaction) {
-        this.incomeList.unshift(transaction);
-    }
-
-    addExpenseTransaction(transaction) {
-        this.expenseList.unshift(transaction);
-    }
-
-    getTransactions() {
-        return this.transactionList;
     }
 
     getIncomeTransactions() {
@@ -371,7 +34,7 @@ class Budget {
     }
 
     displayTransactions(displayContainer, transactionList) {
-        console.log(`Looping through ${transactionList}`);
+        // looping through transaction list to display
         displayContainer.innerText = '';
         transactionList.forEach(transaction => {
             const transactionContainer = document.createElement('div');
@@ -394,9 +57,6 @@ class Budget {
         });
     }
 
-    // based on transaction type: budget += income, budget -= expense => save budget (will use to display)
-    // save income: income += amount (will use to display)
-    // save expense: expense += amount (will use to display)
     getTotalBudget(transaction) {
         // this method will return the total budget
         // add income into total budget
@@ -405,33 +65,26 @@ class Budget {
         console.log(transactionType);
         const amount = Number(Object.values(transaction)[2]);
         if (transactionType === 'income') {
-            // console.log(`${transactionType} is an income`);
-            // console.log(`Current Balance: ${this.budget} + Adding ${amount}`);
-            // console.log(`Current Income: ${this.income} + Adding ${amount}`);
             this.budget += amount;
             this.income += amount;
-            // console.log(`Updated Budget: ${this.budget}; Updated Income: ${this.income}`);
         } else if (transactionType === 'expense') {
-            // console.log(`${transactionType} is an expense`);
-            // console.log(`Current Balance: ${this.budget} + Subtracting ${amount}`);
-            // console.log(`Current Expense: ${this.expense} + Adding ${amount}`);
             this.budget -= amount;
             this.expense += amount;
-            // console.log(`Updated Budget: ${this.budget}; Updated Expense: ${this.expense}`);
         }
         return this.budget;
     }
 
     getIncome() {
+        // used to display total income
         return this.income;
     }
 
     getExpense() {
+        // used to display total expenses
         return this.expense;
     }
 }
 
-// on page load, create budget object
 const addTransactionBtn = document.getElementById('expense-add');
 const addTransactionForm = document.getElementById('new-transaction__container');
 const cancelTransactionBtn = document.getElementById('close-new-transaction');
@@ -441,10 +94,10 @@ const incomeDisplay = document.getElementById('income-display');
 const expenseDisplay = document.getElementById('expense-display');
 const transactionsDisplay = document.getElementById('transaction-display');
 
-// let transactionList;
+// create budget object
+// create variables to store and dynamically use budget properties
 let balance = 0, transactionList = [], income = 0, expense = 0, incomeList = [], expenseList = [];
 const budgetTracker = new Budget(balance, transactionList, income, expense, incomeList, expenseList);
-
 const transactionTypeInput = document.getElementById('transaction-type');
 const descriptionInput = document.getElementById('description');
 const amountInput = document.getElementById('amount');
@@ -452,8 +105,8 @@ const invalidTransactionType = document.getElementById('transaction-type__invali
 const invalidDescription = document.getElementById('description__invalid');
 const invalidAmount = document.getElementById('amount__invalid');
 const mobileView = window.matchMedia('(max-width:  601px)');
-// every form submission => take input values as an object
 
+// reset form display
 function emptyForm() {
     transactionTypeInput.classList.remove('invalid');
     descriptionInput.classList.remove('invalid');
@@ -509,14 +162,12 @@ addTransactionForm.addEventListener('submit', function (event) {
     }
 
     if (isValid) {
+        // every form submission => take input values as an object
         const formData = new FormData(addTransactionForm); // get form's input fields and their values as key/value pairs
         const transaction = Object.fromEntries(formData.entries()); // convert key/value pairs into an object
-        // console.log(transaction);
         budgetTracker.addTransaction(transaction); // add form data into transaction array (will use to display into transactions list)
-        console.log(budgetTracker.transactionList);
-        // addTransactionForm.style.display = 'none';
 
-        budgetTracker.displayTransactions(transactionsDisplay, transactionList); // display only after submit so not running every submit (no duplicates)
+        budgetTracker.displayTransactions(transactionsDisplay, transactionList);
 
         // display balance
         // display income
@@ -524,9 +175,6 @@ addTransactionForm.addEventListener('submit', function (event) {
         balance = budgetTracker.getTotalBudget(transaction);
         income = budgetTracker.getIncome();
         expense = budgetTracker.getExpense();
-        console.log(`Current Balance: ${balance}`);
-        console.log(`Current Income: ${income}`);
-        console.log(`Current Expense: ${expense}`);
         balanceDisplay.innerText = `$${balance}`;
         incomeDisplay.innerText = `$${income}`;
         expenseDisplay.innerText = `$${expense}`;
@@ -537,6 +185,8 @@ addTransactionForm.addEventListener('submit', function (event) {
     }
 })
 
+// ui js
+// prevent form from disappearing on different screen size
 function windowResize(width) {
     if (width.matches) {
         addTransactionForm.style.display = 'none';
@@ -546,7 +196,6 @@ function windowResize(width) {
 windowResize(mobileView);
 mobileView.addEventListener('change', windowResize(mobileView));
 
-// mobile ui js
 addTransactionBtn.addEventListener('click', () => {
     addTransactionForm.style.display = 'flex';
     formOveray.style.display = 'block';
@@ -606,5 +255,3 @@ for (let i = 0; i < indicators.length; i++) {
         indicators[i].classList.add('active');
     });
 }
-
-// desktop ui js
